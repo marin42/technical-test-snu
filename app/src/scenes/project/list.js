8fe7dafc93ef0e21548884c655065753336c9826
Marin <kalasasta@gmail.com>
2023-10-02 21:35:16 +0200

    fix(misc): Added redirection on project detail view after create.

--- app/src/scenes/project/list.js
+++ app/src/scenes/project/list.js
@@ -95,6 +95,8 @@ const Budget = ({ project }) => {
 const Create = ({ onChangeSearch }) => {
   const [open, setOpen] = useState(false);
 
+  const history = useHistory();
+
   return (
     <div className="mb-[10px] ">
       <div className="flex justify-between flex-wrap">
@@ -146,6 +148,7 @@ const Create = ({ onChangeSearch }) => {
                   if (!res.ok) throw res;
                   toast.success("Created!");
                   setOpen(false);
+                  history.push(`/project/${res.data._id}`);
                 } catch (e) {
                   console.log(e);
                   toast.error("Some Error!", e.code);
